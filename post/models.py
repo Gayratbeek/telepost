9af4cadc88c51816f4121b7c41e38b83336180a7
Gayrat <gayratbek.sultonov@gmail.com>
2020-07-16 00:13:40 +0500

    Initial commit

---
+++ post/models.py
@@ -0,0 +1,107 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Категория поста"""
+    name = models.CharField("Категория", max_length=64)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Magazine(models.Model):
+    """Магазин"""
+    name = models.CharField("Название магазина", max_length=32)
+    telelink = models.CharField("Ссылка на телеграм", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Магазин"
+        verbose_name_plural = "Магазины"
+
+
+class Post(models.Model):
+    """Пост"""
+    title = models.CharField("Название", max_length=64)
+    poster = models.ImageField("Главное фото", upload_to="product_images/")
+    description = models.TextField("Описание")
+    price = models.PositiveIntegerField("Стоимость", default=0, help_text="Указывать в сумах")
+    magazine = models.ForeignKey(Magazine, verbose_name="Поставщик",
+                                 on_delete=models.SET_DEFAULT, default='Magazine')
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=100, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+
+class PostImages(models.Model):
+    """Изображения поста в большом количестве"""
+    title = models.CharField("Заголовок", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="product_images/")
+    post = models.ForeignKey(Post, verbose_name="Пост", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Фотки поста"
+        verbose_name_plural = "Фотки поста"
+
+
+class RatingStar(models.Model):
+    """Звезда рейтинга"""
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезды рейтинга"
+
+
+class Rating(models.Model):
+    """Звезда рейтинга"""
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
+    post = models.ForeignKey(Post, on_delete=models.CharField, verbose_name="пост")
+
+    def __str__(self):
+        return f"{self.star} - {self.post}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+
+class Reviews(models.Model):
+    """Отзывы"""
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey('self', verbose_name="Родитель",
+                               on_delete=models.SET_NULL, blank=True, null=True)
+    post = models.ForeignKey(Post, verbose_name="пост", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.post}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
