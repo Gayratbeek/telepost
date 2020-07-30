e77e6f109cae171fa743148da6d1005d1bd43021
Gayrat <gayratbek.sultonov@gmail.com>
2020-07-30 16:12:17 +0500

    Вторичный темплейт (изменил продакт детейл) #Вывод всех товаров в детейл маркет + Вывод всех товаров в все товары

--- post/urls.py
+++ post/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path("post/", views.PostView.as_view()),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path("", views.home, name='home'),
     path("review/<int:pk>/", views.AddReview.as_view(), name='add_review'),
     path("about/<str:slug>", views.MagazineView.as_view(), name='magazine_detail'),
     path('market/<str:username>', views.MagazinePostListView.as_view(), name='magazine_posts'),
